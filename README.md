068d8aeb21c71409f6cc7f0df404e35fb7b1408c
Philipp Söder <philippsoeder@gmx.de>
2022-10-02 00:20:20 +0200

    Add very simple REST service

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Go-REST-service
-This is a simple rest service written in go, not super useful, just something for me to get used to go.
+This is a simple rest service written in go, not super useful, just something 
+for me to get used to go.
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+	log.Printf("Endpoint Hit: helloWorld")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", helloWorld)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func main() {
+	handleRequests()
+}
